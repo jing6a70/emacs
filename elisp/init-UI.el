9df5f343f15df0cf357ea616bd1735f268de27ee
jingpeng <820151608@qq.com>
2025-03-11 16:16:02 +0800

    firaCode字体删除与文件权限更改

--- elisp/init-UI.el
+++ elisp/init-UI.el
@@ -25,7 +25,7 @@
 ;; 字体与字体大小设置
 ;; 如果没有安装; 请安装FiraCode字体:
 ;; ArchLinux: yay -S nerd-fonts-fira-code
-(set-frame-font "FiraCode Nerd Font-12")
+;; (set-frame-font "FiraCode Nerd Font-12")
 ;; 单设置字体大小(set-face-attribute 'default nil :height 135)
 
 ;; 设置emacs的主题UI Settings for UI theme
